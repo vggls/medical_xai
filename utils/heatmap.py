2410ee9716d5595f3773473e089f27d4b6b34406
Vaggelis Lamprou <55101427+vggls@users.noreply.github.com>
2023-05-27 18:44:58 +0300

    Update heatmap.py

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -1,24 +1,24 @@
 '''
-ATTRIBUTIONS
-    => HiResCAM algorithm
-        a) GitHub: https://github.com/jacobgil/pytorch-grad-cam
-        b) Scientific publication: https://arxiv.org/pdf/2011.08891.pdf
-        c) example code:
-             # pip install grad-cam
+Calculating the pixel_attributions for the HiResCAM method:
+        a) example code:
+             pip install grad-cam # see https://github.com/jacobgil/pytorch-grad-cam
              from pytorch_grad_cam import HiResCAM
-             cam = HiResCAM(model=model, target_layers=target_layers, use_cuda=True)
+             cam = HiResCAM(model=model, 
+                            target_layers=target_layers, 
+                            use_cuda=True)
              #targets = [ClassifierOutputTarget(class_of_interest_as_integer))]
-             pixel_attributions = cam(input_tensor=tensor.unsqueeze(0), targets=None )[0,:,:]  # numpy.ndarray, 2-dim, values in [0,1]
-        d) Remarks on the target_layer attribute:
+             pixel_attributions = cam(input_tensor=tensor.unsqueeze(0), 
+                                        targets=None )[0,:,:]  # numpy.ndarray, 2-dim, values in [0,1]
+        b) on the target_layer attribute of the HiResCAM instance:
             - It is the layer wrt which we compute the class derivatives
             - Usually this will be the last convolutional layer in the model.
               In this case, as remarked in Github by the authors, some common choices can be:
               Resnet18 and 50: [model.layer4[-1]]
               vgg16, vgg19, densenet161, densenet201, efficientnets, mobilenet_v2, mobilenet_v3_small, mobilenet_v3_large: [model.features[-1]]
               mnasnet1_0: model.layers[-1]     
-        e) VERY IMPORTANT: In case the target layer was frozen for the training phase, we should unfreeze it in order to pass the model as HiResCAM argument.
+        c) VERY IMPORTANT: In case the target layer was frozen for the training phase, we should unfreeze it in order to pass the model as HiResCAM argument.
                            Otherwise, TypeError occurs.  
-        f) Remark on the 'targets' parameter of the HiResCAM methdod:
+        d) Remark on the 'targets' parameter:
              As per lines 105-110 of https://github.com/jacobgil/pytorch-grad-cam/blob/master/cam.py
              above code returns only the heatmap of the highest prob category (targets=None by default).
              BUT in order to get the heatmaps from the rest classes (if necessary) one of the following Target classes should be used
